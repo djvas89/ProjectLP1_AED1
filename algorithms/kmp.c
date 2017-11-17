be305d8db326be3097af5e066ac005cfd4370b64
Diogo José Soares <djvas89@gmail.com>
2017-11-17 13:47:04 +0000

    Update kmp.c

--- algorithms/kmp.c
+++ algorithms/kmp.c
@@ -2,53 +2,83 @@
 // Created by djvas on 16/11/2017.
 //
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define XSIZE 10
-void preKMP(char *x, int m, int kmpNext[]);
-void KMP(char *x, int m, char *y, int n);
+void failure(char* pattern, int* f);
+int kmp(char* t, char* p);
 
+int* init_array(int size) {
+  int* arr = (int*)malloc(size * sizeof(int));
+  int i;
+  for(i = 0; i < size; i++) {
+    arr[i] = 0;
+  }
 
-void preKMP(char *x, int m, int kmpNext[])
-{
-    int i, j;
-
-    i = 0;
-    j = kmpNext[0] = -1;
-    while (i < m)
-    {
-        while (j > -1 && x[i] != x[j])
-            j = kmpNext[j];
-        i++;
-        j++;
-        if (x[i] == x[j])
-            kmpNext[i] = kmpNext[j];
-        else
-            kmpNext[i] = j;
-    }
+  return arr;
 }
 
+int main(void) {
+  char* pattern = "abacab";
+  char* text = "bbacbabcbabcbabbabcbabcbacbbbacbacbacbacbacbabacab";
 
-void KMP(char *x, int m, char *y, int n)
-{
-    int i, j, kmpNext[XSIZE];
+  int match = kmp(text, pattern);
 
-    /* Preprocessing */
-    preKMP(x, m, kmpNext);
+  printf("Match at: %d\n", match);
 
-    /* Searching */
-    i = j = 0;
-    while (j < n)
-    {
-        while (i > -1 && x[i] != y[j])
-            i = kmpNext[i];
-        i++;
-        j++;
-        if (i >= m)
-        {
-            OUTPUT(j - i);
-            i = kmpNext[i];
-        }
+  return 0;
+}
+
+int kmp(char* t, char* p) {
+  int m = strlen(p);
+  int n = strlen(t);
+
+  int* f = init_array(m); // Failure function values.
+  int i = 0;
+  int j = 0;
+
+  while (i < n) {
+    if (t[i] == p[j]) {
+      if (j == m - 1) {
+        return i - j;
+      }
+      else {
+        i += 1;
+        j += 1;
+      }
     }
+    else {
+      if (j > 0) {
+        j = f[j-1];
+      }
+      else {
+        i += 1;
+      }
+    }
+  }
+
+  return -1;
+}
+
+void failure(char* p, int* f) {
+  f[0] = 0;
+  int i = 1;
+  int j = 0;
+
+  int m = strlen(p);
+
+  while (i < m) {
+    if (p[i] == p[j]) {
+      f[i] = j + 1; // j+1 matches up to the current character.
+      i += 1;
+      j += 1;
+    }
+    else if (j > 0) {
+      j = f[j - 1];
+    }
+    else {
+      f[i] = 0;
+      i += 1;
+    }
+  }
 }
