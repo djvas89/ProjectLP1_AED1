5a4ec8bae6fe664e66b8f93fd930965a72dc8aa5
Diogo José Soares <djvas89@gmail.com>
2017-11-18 20:21:30 +0000

    Update firstPhase.c

--- src/firstPhase.c
+++ src/firstPhase.c
@@ -69,11 +69,11 @@ typedef void(*split_fn)(const char *, size_t, void *);
  
 void split(const char *str, char sep, split_fn fun, void *data)
 {
-    unsigned int start = 0, stop;
-    for (stop = 0; str[stop]; stop++) {
-        if (str[stop] == sep) {
-            fun(str + start, stop - start, data);
-            start = stop + 1;
+    unsigned int start = 0, i;
+    for (i = 0; str[i]; i++) {
+        if (str[i] == sep[i]) { //if finds any delimiter
+            fun(str + start, i - start, data);
+            start = i + 1;
         }
     }
     fun(str + start, stop - start, data);
